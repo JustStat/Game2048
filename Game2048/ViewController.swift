7957e383417ebb8a2e3fb44099dffeb9325ee889
Kirill Varlamov <varlamov@farpost.com>
2019-05-09 15:32:55 +1000

    Initial Commit

---
+++ Game2048/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game2048
+//
+//  Created by Kirill Varlamov on 09/05/2019.
+//  Copyright © 2019 Kirill Varlamov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
