4b5decabd11609adad48dc88252a3d893d2f199c
Kirill Varlamov <varlamov@farpost.com>
2019-05-09 18:48:16 +1000

    Add app palette

---
+++ Game2048/Resources/Colors.swift
@@ -0,0 +1,89 @@
+//
+//  Colors.swift
+//  Game2048
+//
+//  Created by Kirill Varlamov on 09/05/2019.
+//  Copyright © 2019 Kirill Varlamov. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class var background: UIColor {
+        get {
+            return UIColor(red: 0.949, green: 0.953, blue: 0.961, alpha: 1.0)
+        }
+    }
+    
+    class var cellBackground: UIColor {
+        get {
+            return UIColor(red: 0.729, green: 0.756, blue: 0.796, alpha: 1.0)
+        }
+    }
+    
+    class var color2: UIColor {
+        get {
+            return UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
+    class var color4: UIColor {
+        get {
+            return UIColor(red: 0.882, green: 0.890, blue: 0.902, alpha: 1.0)
+        }
+    }
+    
+    class var color8: UIColor {
+        get {
+            return UIColor(red: 0.659, green: 0.863, blue: 0.984, alpha: 1.0)
+        }
+    }
+    
+    class var color16: UIColor {
+        get {
+            return UIColor(red: 0.565, green: 0.698, blue: 0.976, alpha: 1.0)
+        }
+    }
+    
+    class var color32: UIColor {
+        get {
+            return UIColor(red: 0.259, green: 0.537, blue: 0.894, alpha: 1.0)
+        }
+    }
+    
+    class var color64: UIColor {
+        get {
+            return UIColor(red: 0.161, green: 0.392, blue: 0.812, alpha: 1.0)
+        }
+    }
+    
+    class var color128: UIColor {
+        get {
+            return UIColor(red: 0.110, green: 0.290, blue: 0.694, alpha: 1.0)
+        }
+    }
+    
+    class var color256: UIColor {
+        get {
+            return UIColor(red: 0.141, green: 0.243, blue: 0.502, alpha: 1.0)
+        }
+    }
+    
+    class var color512: UIColor {
+        get {
+            return UIColor(red: 0.078, green: 0.173, blue: 0.502, alpha: 1.0)
+        }
+    }
+    
+    class var color1024: UIColor {
+        get {
+            return UIColor(red: 0.016, green: 0.118, blue: 0.694, alpha: 1.0)
+        }
+    }
+    
+    class var color2048: UIColor {
+        get {
+            return UIColor(red: 0.059, green: 0.039, blue: 420, alpha: 1.0)
+        }
+    }
+}
--- Game2048/ViewController.swift
+++ Game2048/ViewController.swift
@@ -12,7 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    func setupView() {
+        view.backgroundColor = .background
     }
 
 
